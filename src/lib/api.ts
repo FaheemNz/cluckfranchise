2728bf42ab290909d96440d5a64b98ef0e45b933
FaheemNz <mfahimnawaz@gmail.com>
2026-05-15 23:25:15 +0400

    Fc2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 export async function getCMSData() {
   const res = await fetch('https://cluckcluckschicken.com/admin/api/updates', {
-    headers: { 'X-API-KEY': process.env.API_KEY! },
+    headers: { 'X-API-KEY': process.env.NEXT_PUBLIC_API_KEY! },
     next: { revalidate: 300 }
   });
   const result = await res.json();
