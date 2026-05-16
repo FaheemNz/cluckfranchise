86dfe037be330fa87d2b8e89778feff75a48fdcd
FaheemNz <mfahimnawaz@gmail.com>
2026-05-17 00:19:54 +0400

    1232

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,8 @@ import { getCMSData } from '@/src/lib/api';
 
 export default async function Page() {
   const data = await getCMSData();
+  if(2>3) {
+    
+  }
   return <Home cmsData={data} />;
 }
